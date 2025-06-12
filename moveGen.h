55076e8f7bec96adeb1024adf0cb4817580ae02d
relka61 <157570128+relka61@users.noreply.github.com>
2025-06-13 07:58:13 +1000

    Delete moveGen.h

--- moveGen.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "utilities.h"
-#include <vector>
-
-std::vector<Move> generateMoves(gameState state);
-void makeMove(gameState& state, std::vector<Move>& validMoves, const Move& move);
