8300dc6f9d1e5c76d1ced962cdb150e6c98902cb
relka61 <157570128+relka61@users.noreply.github.com>
2025-06-13 07:58:44 +1000

    Delete playerControls.h

--- playerControls.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-#include "utilities.h"
-#include <vector>
-
-void handlePlayerControls(gameState& state, std::vector<Move>& validMoves, int &selectedSquare, bool isWhiteAtTop);
